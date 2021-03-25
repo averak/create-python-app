a4b78850a0482452ee302bdc8576d784aa092300
averak <abe12@mccc.jp>
2021-03-26 03:35:01 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# create python app
+
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+CLI tool to quickstart Python app.
+
+## Requirement
+
+- Python3
+
+## Installation
+
+```sh
+$ git clone <this repo>
+$ cd <this repo>
+
+$ pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="create_python_app",
+    version="0.1.0",
+    description="CLI tool to quickstart Python app.",
+    license="MIT",
+    author="averak",
+    packages=find_packages(),
+    package_data={'create_python_app': ['template']},
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={
+        'console_scripts': [
+            'create_python_app=create_python_app.core:main',
+        ]
+    },
+)
